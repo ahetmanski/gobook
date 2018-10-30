199af51df9d42d5fe6cee0711d6cccc9959b184c
Andrei Hetmanski <andrei.hetmanski@gmail.com>
2018-10-30 11:18:27 +0300

    Show whole file reading in duplicates example
    It introduces deplicates version 3.

---
+++ ch1/dup3/dup.go
@@ -0,0 +1,29 @@
+// Dup3 finding lines duplicates in files.
+// It reads whole file and process it iteratively.
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+func main() {
+	counts := make(map[string]int)
+	for _, filename := range os.Args[1:] {
+		data, err := ioutil.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "sub3: %v\n", err)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") {
+			counts[line]++
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
