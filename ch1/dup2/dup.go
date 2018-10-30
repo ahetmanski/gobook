3ec8de788ea435c943b8c3678bff1530c4f71a66
Andrei Hetmanski <andrei.hetmanski@gmail.com>
2018-10-30 10:46:59 +0300

    Add second 'find duplicates' version
    It contains files input support.

---
+++ ch1/dup2/dup.go
@@ -0,0 +1,40 @@
+// Dup2 reads standard input and files.
+// Counts lines duplicates and prints
+// duplicates number and lines.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, name := range files {
+			f, err := os.Open(name)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%v\n", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
